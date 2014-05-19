aeb3e4bc3dff8688bca3a24c585841a795d7426e
Cawarnold <christian@thisisglow.com>
2014-05-19 23:44:00 +0100

    final function

---
+++ Assignment1.R
@@ -0,0 +1,25 @@
+#### Programming Assignment 1: Air Pollution ####
+
+dataset_url1 <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
+download.file(dataset_url1, "specdata.zip")
+specdata <- unzip("specdata.zip", exdir = "specdata")
+#list.files("specdata/specdata")
+
+#files <- list.files("specdata/specdata", full.names = TRUE)
+#files
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  files <- list.files(directory, full.names = TRUE)  ## creates a list of files
+  dat <- data.frame()  #creates an empty data frame
+  for (i in id) {
+    dat <- rbind(dat, read.csv(files[i]))
+  }
+  dat_subset <- dat[dat[, "ID"] == id, ]
+  meanOfPollutant <- mean(dat_subset$pollutant, na.rm = TRUE)
+  meanOfPollutant
+}
+
+
+
+
+
