65bbaed257df83654b3d0e6ea21f185c14644921
Cawarnold <christian@thisisglow.com>
2014-05-19 19:15:33 +0100

    Practice stuff

--- Examples_A2.R
+++ Examples_A2.R
@@ -6,9 +6,25 @@
 
 ##Part 1
 
+dataset_url <- "http://dl.dropboxusercontent.com/u/8036886/diet_data.zip"
+download.file(dataset_url, "diet_data.zip")
+unzip("diet_data.zip", exdir = "diet_data")
+
+
+dataset_url1 <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
+download.file(dataset_url1, "specdata.zip")
+unzip("specdata.zip", exdir = "specdata")
+
+list.files("spec_data")
+
+andy <- read.csv("diet_data/Andy.csv")
+
+R.version.string
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
+  
   
   ## 'pollutant' is a character vector of length 1 indicating
   ## the name of the pollutant for which we will calculate the
@@ -20,3 +36,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 }
+
+C:\Users\Christian\Desktop\Christian\R\RProgramming\rprog-data-specdata.zip\
+
+pollutantmean(directory, pollutant, id(range))
+
+pollutantmean(C:\Users\Christian\Desktop\Christian\R\RProgramming\rprog-data-specdata.zip\specdata)
+
