3941b224b19fd5cf820ab07a663cd13b569bc2c1
Cawarnold <christian@thisisglow.com>
2014-05-20 18:31:59 +0100

    updates, nothing new

--- Assignment1.R
+++ Assignment1.R
@@ -3,10 +3,13 @@
 dataset_url1 <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
 download.file(dataset_url1, "specdata.zip")
 specdata <- unzip("specdata.zip", exdir = "specdata")
+specdata1 <- unzip("specdata.zip")
 #list.files("specdata/specdata")
 
-#files <- list.files("specdata/specdata", full.names = TRUE)
-#files
+
+files1 <- list.files("specdata/specdata", full.names = TRUE)
+files2 <- dir("specdata/specdata")
+head(files2)
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   files <- list.files(directory, full.names = TRUE)  ## creates a list of files
@@ -18,7 +21,6 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   meanOfPollutant <- mean(dat_subset$pollutant, na.rm = TRUE)
   meanOfPollutant
 }
-
 
 
 
--- Examples_A2.R
+++ Examples_A2.R
@@ -102,6 +102,18 @@ pollutantmean(directory, pollutant, id(range))
 
 pollutantmean2("specdata", "sulfate", id = 1:10)
 
+#### Trial3 ####
+files <- dir("specdata/specdata", full.names = TRUE) 
+
+
+dat <- data.frame()
+for (i in 68:74) {
+  dat <- rbind(dat, read.csv(files[i]))
+}
+
+
+mean(dat[dat[, "ID"] == 70:72, ]$"nitrate", na.rm = TRUE)
+
 
 #### Test pollutantmean2 ####
 
@@ -176,6 +188,11 @@ if(nobs > threshold){
   sulfate <- filecom[2]
   ok <- rbind(cor(nitrate,sulfate),ok)
 
+  
+  
+
+  
+
 #### Test pollutantmean ####
   
 pollutantmean("specdata", "sulfate", 1:2)
@@ -188,7 +205,53 @@ pollutantmean("specdata/specdata", "nitrate", 23)
   
   
   
+
 ####################################################
 dataset_url <- "http://dl.dropboxusercontent.com/u/8036886/diet_data.zip"
 download.file(dataset_url, "diet_data.zip")
 unzip("diet_data.zip", exdir = "diet_data")
+
+#####################################################
+
+##
+## Implements a set of functions that implement a set of funtions
+##  that create a cacheable matrix [makeCacheMatrix()] and a 
+##  function to solve for the inverse of that matrix and save it 
+##  in the cache. Returning this cached value if called again.
+
+##
+## Create a cached matrix that includes functions to get a 
+##  previously calculated inverse and a function to calculate
+##  the inverse of the matrix and save that value in the cache.
+##
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
+}
+
+
+## This function checks if cached value exists for the matrix 
+##  and returns it if present. If not, it calculates and saves
+##  it in the cache.
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
